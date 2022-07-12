705454243c2c6fbfaec9a7b59d3ffe5585d5622f
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-12 16:01:00 +0500

    Add routing and fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import PostDetail from "./components/PostDetail";
+
+import "./styles/app.css";
+import { URL } from "./constants/utils";
 
 function App() {
-  return <>Hello World</>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(URL + "/posts/published", {
+      method: "GET",
+      headers: { "Content-Type": "Application/json" },
+    })
+      .then((res) => res.json())
+      .then((resData) => {
+        console.log(resData);
+        updatePosts(resData);
+      });
+  }, []);
+
+  const updatePosts = (posts) => {
+    setPosts(posts);
+  };
+
+  return (
+    <div className="app">
+      <Header>
+        <h1>Blogs</h1>
+      </Header>
+
+      <Routes>
+        <Route path="/" element={<Home posts={posts} />} />
+        <Route path="/post/:postId" element={<PostDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
