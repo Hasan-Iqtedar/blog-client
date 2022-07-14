a566fb38808226aa485a584e4e0183abe44fbbd0
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-14 19:06:16 +0500

    Clean code

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,25 +1,12 @@
-import { useEffect, useState } from "react";
 import Card from "./Card";
 import "../styles/comment.css";
-import { URL } from "../constants/utils";
 
 const Comment = ({ comment }) => {
   return (
     <div className="comment">
       <Card style={style}>
         <h4>{comment.author_name}</h4>
-        <p>
-          {comment.text}
-          {/* A very nice post indeed! Eye Opening and wonderful. A very nice post
-          indeed! Eye Opening and wonderful. A very nice post indeed! Eye
-          Opening and wonderful. A very nice post indeed! Eye Opening and
-          wonderful. A very nice post indeed! Eye Opening and wonderful. A very
-          nice post indeed! Eye Opening and wonderful. A very nice post indeed!
-          Eye Opening and wonderful. A very nice post indeed! Eye Opening and
-          wonderful. A very nice post indeed! Eye Opening and wonderful. A very
-          nice post indeed! Eye Opening and wonderful. A very nice post indeed!
-          Eye Opening and wonderful. */}
-        </p>
+        <p>{comment.text}</p>
       </Card>
     </div>
   );
