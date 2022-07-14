f52266a8fefa046ca2b5a657a79642507b55b6ea
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-14 19:04:23 +0500

    Clean code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/blog-client" >
+  <BrowserRouter basename="/blog-client">
     <App />
   </BrowserRouter>
 );
